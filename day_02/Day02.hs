5e90e64155ff2c8fd70c6e4e27a3f7101bc6a609
Levente Kurusa <lkurusa@acm.org>
2018-12-02 12:08:54 +0100

    Add Day2 part B
    Signed-off-by: Levente Kurusa <lkurusa@acm.org>

--- day_02/Day02.hs
+++ day_02/Day02.hs
@@ -2,6 +2,7 @@ import System.IO
 import Control.Monad
 import Debug.Trace
 import Data.List
+import qualified Data.MemoCombinators as Memo
 
 test1 = "abcdef"
 test2 = "bababc"
@@ -11,8 +12,25 @@ test5 = "aabcdd"
 test6 = "abcdee"
 test7 = "ababab"
 
+
+test8 = ["bcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"]
+
 count :: Eq a => a -> [a] -> Int
 count x = length . filter (x ==)
+
+memoString = Memo.list Memo.char
+
+lev' :: String -> String -> Int
+lev' xs ys = levMemo xs ys
+  where levMemo = Memo.memo2 memoString memoString lev
+        lev [] [] = 0
+        lev [] ys = length ys
+        lev xs [] = length xs
+        lev (x:xs) (y:ys)
+          | x == y    = levMemo xs ys
+          | otherwise = 1 + minimum [levMemo xs (y:ys),
+                                     levMemo (x:xs) ys,
+                                     levMemo xs ys]
 
 solvePartOne :: [String] -> Int
 solvePartOne xs
@@ -32,6 +50,12 @@ solvePartOne xs
                       (map (\c -> count c a)
                       a))
   
+-- solvePartTwo :: [String] -> (String, String)
+solvePartTwo xs
+  = (\(a, b, _) -> (a, b)) $ head $ filter (\(a, b, dist) -> dist == 1) distances
+  where
+    distances = map (\(a, b) -> (a, b, lev' a b)) all
+    all = zip (concat $ map (\x -> take (length xs) $ repeat x) xs) (cycle xs)
 
 main :: IO ()
 main = do
@@ -40,5 +64,5 @@ main = do
   let ls = lines contents
   putStr "Part one: "
   print $ solvePartOne ls
-  -- putStr "Part two: "
-  -- print $ solvePartTwo ls
+  putStr "Part two: "
+  print $ solvePartTwo ls
